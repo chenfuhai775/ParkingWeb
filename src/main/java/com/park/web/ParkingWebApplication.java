3185b157a03275ea463f432bb2185bc409aa316a
chenfuhai <chenfuhai775@163.com>
2019-12-11 23:11:02 +0800

    提交代码

---
+++ src/main/java/com/park/web/ParkingWebApplication.java
@@ -0,0 +1,13 @@
+package com.park.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/park/web/ParkingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
